6b668a24857fc43ac05d8486011097ef55709f5a
Yvonne Tang <ytang604@gmail.com>
2020-01-18 23:38:55 +0000

    create searchBar components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import Countries from '../Countries/Countries';
 import Toolbar from '../Toolbar/Toolbar';
+import SearchBar from '../SearchBar/SearchBar';
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
                 <Toolbar />
-                <header className="App-header">
+                <main style={{ marginTop: '56px' }}></main>
+                <SearchBar />
+        
+             <header className="App-header">
                     <h1>REST Countries API</h1>
                     <Countries />
                 </header>
